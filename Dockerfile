35c8ba208f49421f056fd49940201a8ea560ad48
Evan Washkow <epwashkow@gmail.com>
2019-07-04 11:34:13 -0700

    Write provisioning script to create new container

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
 # Fetch latest PHP image
-FROM php:latest
+FROM php:apache
+
+# Expose HTTP
+EXPOSE 80
+
+# Set working directory to web root (affects interactive shell)
+WORKDIR /var/www/html
---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#
+# DESTROY ANY PREVIOUS CONTAINER AND CREATE A NEW ONE
+#
+
+# Stop and destroy previous container
+docker stop php-unit
+docker rm php-unit
+
+# (Re)build latest image
+source $PWD/build.sh
+
+# Start a new container named "php-unit"
+docker run --name php-unit -d -p 80:80 php-unit

eb71b1f8ea8f2129b4be69e2b1fe9a3f0cad4278
Evan Washkow <epwashkow@gmail.com>
2020-03-14 11:56:04 -0700

    Add build for php-libraries:local image

--- local/build.sh
+++ local/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Get the current directory
-currentDir=$(dirname "$0")
+currentDir=$(dirname $(realpath $0))
 
 # Run the build.sh in the parent directory. This will produce the php-libraries:latest Docker image.
 cd ..
 ./build.sh
+
+# Build the image in the current directory
+cd $currentDir
+docker build --tag php-libraries:local .
